b3737f6b0adaa77d01fc8539827d1bb7f2c5c88a
LapTop_Bogdan <bogdanvoinov90@gmail.com>
2025-04-06 23:05:51 +0300

    bug fix

--- main.py
+++ main.py
@@ -12,15 +12,14 @@ from routers.auth.auth import router as auth_router
 
 app = FastAPI()
 router = APIRouter()
-router.include_router(auth_router, prefix="/auth", tags=["auth"])
-app.include_router(router, prefix="/api/v0", tags=["api"])
+router.include_router(auth_router, prefix="/auth")
+app.include_router(router, prefix="/api/v0")
 app.mount("/templates", StaticFiles(directory="templates"), name="templates")
 
 @app.get("/", response_class=JSONResponse)
 async def index(session_data: SessionData = Depends(get_session_data)):
     # навешивание этих аргументов уже значит проверку
     return session_data.dict()
-
 
 
 if __name__ == "__main__":
