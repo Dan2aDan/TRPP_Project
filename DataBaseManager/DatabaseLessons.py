0d5eb1a52da542d9781493a9e4888632c44d3f7e
LapTop_Bogdan <bogdanvoinov90@gmail.com>
2025-04-21 00:54:00 +0300

    add get_lesson_from_student

--- DataBaseManager/DatabaseLessons.py
+++ DataBaseManager/DatabaseLessons.py
@@ -16,7 +16,7 @@ class DatabaseLessons:
 
     def get_lesson_from_student(self, lesson_id):
         query = sqlalchemy.select(LessonsDepends).where(LessonsDepends.lesson_id == lesson_id)
-        return self.db.select(query, types=self.db.all_)
+        return [node.student_id for node in self.db.select(query, types=self.db.all_)]
 
     def get_student_lessons(self, student_id):
         # Получаем teacher_id студента
